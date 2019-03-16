a69fea4cf6cd1aff29b884358574bb71b6da9f48
elukuro <heruhartantot@gmail.com>
2019-03-16 12:54:24 +0700

    try release 100daysofcode

---
+++ 100daysofcode/precache-manifest.83a13a16526baf977bbaec61effc4283.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "256cb8b9886ea5b4a4316f508920971a",
+    "url": "/plant.png"
+  },
+  {
+    "revision": "607fbe42af3c04b79af8",
+    "url": "/js/chunk-vendors.91ab1b01.js"
+  },
+  {
+    "revision": "a3527ae0298a36d8e638",
+    "url": "/js/app.730b004f.js"
+  },
+  {
+    "revision": "afb96755fd1986f00108eff6a1f726b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "256cb8b9886ea5b4a4316f508920971a",
+    "url": "/img/plant.256cb8b9.png"
+  },
+  {
+    "revision": "607fbe42af3c04b79af8",
+    "url": "/css/chunk-vendors.248a60c9.css"
+  },
+  {
+    "revision": "a3527ae0298a36d8e638",
+    "url": "/css/app.ce2fb9ed.css"
+  }
+];
